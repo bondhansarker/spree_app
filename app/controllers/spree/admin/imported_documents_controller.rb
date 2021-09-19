6f27837940fffb55067b42a04031e19c37261898
Bondhan Sarker <bondhansarker100@gmail.com>
2021-09-19 18:20:24 -0400

    feat(import_document): add controller imported_documents
    * generate a controller named imported_documents
    * add templates for the CRUD actions
    * add routes for imported_document resource

---
+++ app/controllers/spree/admin/imported_documents_controller.rb
@@ -0,0 +1,7 @@
+module Spree
+  module Admin
+    class ImportedDocumentsController < ResourceController
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   # We ask that you don't use the :as option here, as Spree relies on it being
   # the default of "spree".
   mount Spree::Core::Engine, at: '/'
+  Spree::Core::Engine.add_routes do
+    namespace :admin, path: Spree.admin_path do
+      resources :imported_documents
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
