52fd1759b8ca72e5d27654a241881a943529bc89
Bondhan Sarker <bondhansarker100@gmail.com>
2021-09-20 17:01:05 -0400

    feat(background job): add a callback method in imported_document model for importing data from attached csv file.
    * add a method which will trigger after saving the imported_document
    * add after_save_commit(callback method) for getting the id of the imported_document object
    * call the sidekiq job asynchronously and send the object id as parameter.

--- app/models/spree/imported_document.rb
+++ app/models/spree/imported_document.rb
@@ -3,5 +3,10 @@ module Spree
     has_one_attached :document
     validates :name, :note, :document, presence: true
     validates :document, content_type:  ["application/csv", "text/csv"]
+    after_save_commit :import_data_from_csv
+
+    def import_data_from_csv
+       ImportDataFromCsvWorker.perform_async(self.id)
+    end
   end
 end
