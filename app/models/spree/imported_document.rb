896289a50ab3604ec636da03373c321012e924ac
Bondhan Sarker <bondhansarker100@gmail.com>
2021-09-23 08:43:12 -0400

    refactor(import_document): change the callback from "after_save_commit" to "after_save" for using transaction

--- app/models/spree/imported_document.rb
+++ app/models/spree/imported_document.rb
@@ -3,7 +3,7 @@ module Spree
     has_one_attached :document
     validates :name, :note, :document, presence: true
     validates :document, content_type:  %w[application/csv text/csv]
-    after_save_commit :import_products_from_csv_file
+    after_save :import_products_from_csv_file
 
     def import_products_from_csv_file
        Import::ProductsWorker.perform_async(self.id)
