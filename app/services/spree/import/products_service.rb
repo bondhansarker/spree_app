e00f3c4e30754ac7b844e6c3db8981150fa2ab2f
Bondhan Sarker <bondhansarker100@gmail.com>
2021-09-25 00:47:42 -0400

    feat(import_document): create product with the data from csv file
    * use default store and shipping categories
    * image is not added yet.

--- app/services/spree/import/products_service.rb
+++ app/services/spree/import/products_service.rb
@@ -19,11 +19,30 @@ module Spree
       end
 
       def extract_data_from_csv
-        data = []
-        CSV.foreach imported_document_path, headers: true do |row|
-          data << row.to_h
+        rows ||= CSV.parse(File.read(imported_document_path), headers: true)
+        rows.each do |title, author, original_price, discounted_price, url, image_url, summary, *properties|
+          product = Spree::Product.where(name: get_value(title).titleize).first_or_create! do |product|
+            product.stores = Spree::Store.all
+            product.shipping_category = Spree::ShippingCategory.find_by!(name: 'Default')
+            product.currency = "BDT"
+            product.price = get_value(original_price)&.gsub(/[\s,TK.]/, "")
+            product.description = get_value(summary)
+            product.author = get_value(author)
+            product.discounted_price = get_value(discounted_price)
+            product.url = get_value(url)
+          end
+          properties.each do |attribute, value|
+            product.set_property(get_property_key(attribute), value)
+          end
         end
-        p data
+      end
+
+      def get_property_key(attribute)
+        attribute.gsub("_in_specification", "").titleize.to_sym if attribute.match?('_in_specification')
+      end
+
+      def get_value(column_with_header)
+        column_with_header.last
       end
     end
   end
